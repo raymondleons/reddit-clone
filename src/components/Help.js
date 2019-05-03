becf3b2df3e49a19a22a70442c8d1dd9e6c993ca
Raymond Leon Sembiring <raymondleons1997@gmail.com>
2019-05-03 18:55:14 +0700

    [Raymond] - adding page help

---
+++ src/components/Help.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Help extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h2><i className="fa fa fa-info-circle" /> Help</h2>
+        <p>This page give you information for use this application</p>
+        <hr/>
+      </div>
+    )
+  }
+}
+
+export default Help;
