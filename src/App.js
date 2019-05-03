f11b61a36d2c192b6a7a7015970a5d95c1a9598f
Raymond Leon Sembiring <raymondleons1997@gmail.com>
2019-05-03 18:08:45 +0700

    [Raymond] - adding provider for redux and add store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import {Provider} from 'react-redux'
+import store from './store'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import './assets/main.scss'
 
@@ -7,20 +9,20 @@ import { Button } from 'reactstrap'
 // Components
 import NavBar from './components/NavBar'
 import Homepage from './components/Homepage'
-import AddTopics from './components/AddTopic'
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
       <div>
         <NavBar/>
           <div>
           <Route exact path="/" component={Homepage} />
-          <Route exact path="/topic" component={AddTopics} />
           </div>
       </div>
       </Router>
+      </Provider>
     )
   }
 }
